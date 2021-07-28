582333022d014117d01c01d4b2fcf4639b6e24dc
Sourabh <sk857087@gmail.com>
2021-07-28 19:37:51 +0530

    dynamic next route

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,9 +1,61 @@
 import styled from "styled-components";
+import {useAuthState} from "react-firebase-hooks/auth"
+import {auth, db} from "../firebase";
+import { useRouter } from "next/router";
+import { Avatar , IconButton} from "@material-ui/core";
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import { useCollection } from "react-firebase-hooks/firestore";
 
-function ChatScreen() {
+function ChatScreen( { chat, messages }) {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  const [messagesSnapshot] = useCollection(
+    db
+    .collection("chats")
+    .doc(router.query.id)
+    .collection('messages')
+    .orderBy('timestamp','asc')
+  );
+
+
+  const showMessages = () => {
+    if(messagesSnapshot) {
+      return messagesSnapshot.docs.map(message => (
+        <Message
+         key={message.id}
+         user={message.data().user}
+         message = {{
+           ...message.data(),
+           timestamp: message.data().timestamp?.toDate().getTime(),
+         }}
+        />
+      ))
+    }
+  }
   return(
     <Container>
-    <h1>This is a Chat</h1>
+     <Header>
+      <Avatar />
+
+        <HeaderInformation>
+        <h3>recipient EMAIL</h3>
+        <p>last seen...</p>
+        </HeaderInformation>
+        <HeaderIcons>
+        <IconButton>
+        <AttachFileIcon />
+        </IconButton>
+        <IconButton>
+        <MoreVertIcon />
+        </IconButton>
+
+        </HeaderIcons>
+       </Header>
+       <MessageContainer>
+       {/* show messages */}
+       <EndOfMessage />
+       </MessageContainer>
     </Container>
 
   )
@@ -12,3 +64,34 @@ function ChatScreen() {
 export default ChatScreen;
 
 const Container = styled.div``;
+
+const Header = styled.div`
+position:sticky;
+background-color:white;
+z-index:100;
+top:0;
+display:flex;
+padding:11px;
+height:80px;
+align-items: center;
+border-bottom: 1px solid whitesmoke;
+`;
+
+const HeaderInformation = styled.div`
+margin-left:15px;
+flex:1;
+
+> h3{
+  margin-bottom: 3px;
+}
+> p {
+  font-size:14px;
+  color:gray;
+}
+`;
+
+const HeaderIcons = styled.div``;
+
+const EndOfMessage = styled.div``;
+
+const MessageContainer = styled.div``;
--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,17 +1,20 @@
 import styled from "styled-components";
+import { useAuthState } from "react-firebase-hooks/auth";
 import Head from "next/head";
 import Sidebar from "../../components/Sidebar";
-import ChatScreen from "../../components/ChatScreen"
-
- function Chat() {
+import ChatScreen from "../../components/ChatScreen";
+import { auth, db } from "../../firebase";
+import getRecipientEmail from "../../utils/getRecipientEmail";
+ function Chat({ chat, messages }) {
+   const [user] = useAuthState(auth);
    return (
      <Container>
      <Head>
-     <title>Chat</title>
+     <title>Chat with {getRecipientEmail(chat.users, user)}</title>
      </Head>
      <Sidebar />
      <ChatContainer>
-     <ChatScreen />
+     <ChatScreen chat = {chat} messages={messages} />
      </ChatContainer>
      </Container>
    )
@@ -19,8 +22,54 @@ import ChatScreen from "../../components/ChatScreen"
 
  export default Chat;
 
+ export async function getServerSideProps(context){
+   const ref = db.collection("chats").doc(context.query.id);
+
+   //  PREP the messages on the server
+   const messagesRes = await ref
+   .collection("messages")
+   .orderBy("timestamp", "asc")
+   .get();
+
+   const messages = messagesRes.docs.map(doc => ({
+     id : doc.id,
+     ...doc.data()
+   })).map(messages => ({
+     ...messages,
+     timestamp:messages.timestamp.toDate().getTime()
+   }));
+
+   // PREP the Chats
+   const chatRes = await ref.get();
+
+   const chat = {
+     id : chatRes.id,
+     ...chatRes.data()
+   }
+
+   console.log(chat, messages);
+
+   return {
+     props : {
+       messages: JSON.stringify(messages),
+       chat: chat
+     }
+   }
+ }
+
 const Container = styled.div`
 display:flex;
 `;
 
-const ChatContainer = styled.div``;
+const ChatContainer = styled.div`
+flex:1;
+overflow: scroll;
+height:100vh;
+
+ ::-webkit-scrollbar {
+   display:none;
+ }
+
+  -ms-overflow-style:none; /* IE and EDGE */
+ scrollbar-width:none;    /* Firefox */
+`;
