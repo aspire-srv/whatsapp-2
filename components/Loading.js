13ced5d79e957cfa9b88fe160e56f33a6544c893
Sourabh <sk857087@gmail.com>
2021-07-29 14:33:20 +0530

    vercel

--- components/Loading.js
+++ components/Loading.js
@@ -1,12 +1,11 @@
 import { Circle } from "better-react-spinkit";
-import Image from "next/image";
 
 function Loading () {
   return (
 
     <center style={{display:"grid", placeItems: "center", height: "100vh" }}>
     <div>
-    <Image
+    <img
     src="https://i.pinimg.com/originals/79/dc/31/79dc31280371b8ffbe56ec656418e122.png"
     // style={{ marginBottom: 10 }}
     height= {200}
